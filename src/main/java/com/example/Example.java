9b959dd6358cdd804f6a5376b779fbd9a8c0912f
Aussied <88702612+AussiedDev@users.noreply.github.com>
2022-01-25 20:25:38 +0800

    Update Example.java

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -7,7 +7,6 @@ public class Example {
     public static final Example instance = new Example();
     public Mods modManager;
 
-    /** TODO: Make a optimized and simple base and release it on github (USE ABSTRACTS) */
     public void onStart() {
         String name = "Example", build = "1.0";
         Display.setTitle(name + " " + build);
